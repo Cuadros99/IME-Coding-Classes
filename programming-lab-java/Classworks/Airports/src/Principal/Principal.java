2d368a7caaf3a992df18c795dd25db784222e2f9
Cuadros99 <dany@Cuadros-Desktop.localdomain>
2022-09-17 23:48:13 -0300

    create the menu

--- programming-lab-java/Classworks/Airports/src/Principal/Principal.java
+++ programming-lab-java/Classworks/Airports/src/Principal/Principal.java
@@ -1,6 +1,5 @@
 package Principal;
 
-import Dijkstra.*;
 import java.util.*;
 
 public class Principal {
@@ -9,32 +8,9 @@ public class Principal {
         ArrayList<Airport> airportsList = new ArrayList<Airport>();
         airportsList = ReadCSV.getAirportsData();
 
-        Graph airportsGraph = new Graph(airportsList);
-        
+        Menu mainMenu = new Menu(airportsList);
 
-        String originCode = "GRU";
-        String destinyCode = "GIG";
-        
-        // Node destinyNode = DijkstraAlgorithm.calculateShortestPathFromSource(airportsGraph, originAp.getCode(), destinyAp.getCode());
-        Node destinyNode = DijkstraAlgorithm.calculateShortestPathFromSource(airportsGraph, originCode, destinyCode);
-        
+        mainMenu.showMenu();
 
-        // System.out.printf("\nTrecho %s -> %s\n", originAp.getCode(), destinyAp.getCode());
-        System.out.printf("\nTrecho %s -> %s\n", originCode, destinyCode);
-        System.out.println("---------------------");
-        System.out.print("| ");
-
-        for(Node apNode: destinyNode.getShortestPath()) {
-            System.out.print(apNode.getAirport().getCode());
-            System.out.print(" -> ");
-        }
-        System.out.println(destinyCode);
-        System.out.printf("| %.2f Km\n",destinyNode.getDistance());
-
-        // Node middleNode = airportsGraph.getNodeByCode("RBR");
-        // Double d1 = originAp.getDirectDistanceBetween(middleNode.getAirport());
-        // Double d2 = middleNode.getAirport().getDirectDistanceBetween(destinyAp);
-
-        // System.out.printf("\n%.2f + %.2f = %.2f\n", d1, d2, d1+d2);
     }
 }
